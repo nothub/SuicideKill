84227b1051f3bcf6c6d035e9bd96905db7d56c99
blockparole <>
2019-08-08 23:44:22 +0200

    Release 1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# SuicideKill
+<a href="https://github.com/blockparole/SuicideKill/releases/latest" alt="Download"><img src="https://img.shields.io/github/downloads/blockparole/SuicideKill/latest/total.svg?label=download%20latest&style=popout-square" /></a>
+<a href="https://github.com/blockparole/SuicideKill" alt="Download"><img src="https://img.shields.io/github/languages/code-size/blockparole/SuicideKill.svg?label=repo%20size&style=popout-square" /></a>
+
+A simple Plugin that kills Players with insufficient permissions when they run: `/kill`  
+For Players with correct permissions, the command works as intended: `kill <target>`.
+
+Uses [vanish](https://hub.spigotmc.org/javadocs/spigot/org/bukkit/entity/Player.html#hidePlayer-org.bukkit.plugin.Plugin-org.bukkit.entity.Player-) to mitigate [Coordinate Exploitation](https://2b2t.miraheze.org/wiki/Coordinate_Exploits#Debug_Exploit/).
+
+This Plugin was tested on [Paper](https://papermc.io/) [1.12.2](https://papermc.io/downloads#Paper-1.12/)
+
+Inspired by: [OldschoolKill](https://www.spigotmc.org/resources/oldschoolkill.4047/)
---
+++ src/main/java/not/hub/suicidekill/SuicideKill.java
@@ -0,0 +1,111 @@
+package not.hub.suicidekill;
+
+import not.hub.suicidekill.tasks.CooldownRunnable;
+import not.hub.suicidekill.tasks.UnvanishRunnable;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+public final class SuicideKill extends JavaPlugin implements Listener {
+
+    private String cooldownMessage;
+    private int cooldownValue;
+    private int unvanishDelay;
+
+    private boolean cooldown;
+
+    @Override
+    public void onEnable() {
+        loadConfig();
+        cooldownMessage = getConfig().getString("global-cooldown-message");
+        cooldownValue = getConfig().getInt("global-cooldown-ticks");
+        unvanishDelay = getConfig().getInt("unvanish-delay-ticks");
+        if (unvanishDelay == 0) {
+            unvanishDelay = 1;
+        }
+        BukkitTask cooldownTask = new CooldownRunnable(this).runTaskTimer(this, 0, cooldownValue);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+
+        String cmd = event.getMessage().toLowerCase();
+
+        // not our command
+        if ((!cmd.startsWith("/kill ")) && (!cmd.equals("/kill"))) {
+            return;
+        }
+
+        Player requester = event.getPlayer();
+
+        // issuer has to be a player
+        if (requester == null) {
+            return;
+        }
+
+        // if perm is correct, return so /kill $1 command can be used
+        if (requester.hasPermission("minecraft.command.kill")) {
+            return;
+        }
+
+        // cancel original event
+        event.setCancelled(true);
+
+        // no kills while cooldown active
+        if (cooldown) {
+            cooldownMessage(requester);
+            return;
+        } else {
+            cooldown = true;
+        }
+
+        // tp exploit protection vanish
+        vanish(requester);
+
+        // kill requester
+        requester.setHealth(0);
+        // TODO - EntityDamageEvent.DamageCause.SUICIDE
+
+        // unvanish requester after n ticks
+        BukkitTask unvanishTask = new UnvanishRunnable(this, requester).runTaskLater(this, unvanishDelay);
+
+    }
+
+    public void disableCooldown() {
+        cooldown = false;
+    }
+
+    public void unvanish(Player player) {
+        for (Player onlinePlayer : getServer().getOnlinePlayers()) {
+            if (!onlinePlayer.equals(player)) {
+                onlinePlayer.showPlayer(this, player);
+            }
+        }
+    }
+
+    private void vanish(Player player) {
+        for (Player onlinePlayer : getServer().getOnlinePlayers()) {
+            if (!onlinePlayer.equals(player)) {
+                onlinePlayer.hidePlayer(this, player);
+            }
+        }
+    }
+
+    private void cooldownMessage(Player player) {
+        player.sendMessage(cooldownMessage);
+    }
+
+    private void loadConfig() {
+        getConfig().addDefault("global-cooldown-message", "Sorry, Death is busy at the moment. Please try again later...");
+        getConfig().addDefault("global-cooldown-ticks", 60);
+        getConfig().addDefault("unvanish-delay-ticks", 20);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+}
