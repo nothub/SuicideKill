c7561f9e1cacc35c02fe9a01a3a51db16045e922
blockparole <48992448+blockparole@users.noreply.github.com>
2021-01-14 03:55:01 +0100

    1.3

--- src/main/java/not/hub/suicidekill/Plugin.java
+++ src/main/java/not/hub/suicidekill/Plugin.java
@@ -1,116 +1,139 @@
 package not.hub.suicidekill;
 
-import not.hub.suicidekill.tasks.CooldownRunnable;
-import not.hub.suicidekill.tasks.UnvanishRunnable;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
-public final class SuicideKill extends JavaPlugin implements Listener {
+public final class Plugin extends JavaPlugin implements Listener {
 
-    private boolean cooldown;
+    private final Set<UUID> cooldowns = ConcurrentHashMap.newKeySet();
+    private final Set<UUID> vanished = ConcurrentHashMap.newKeySet();
 
     @Override
     public void onEnable() {
         config();
-        new CooldownRunnable(this).runTaskTimer(this, 0, getConfig().getInt("cooldown-ticks"));
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer()
+                .getPluginManager()
+                .registerEvents(this, this);
     }
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    @Override
+    public void onDisable() {
+        vanished
+                .stream()
+                .map(uuid -> getServer().getPlayer(uuid))
+                .forEach(this::unvanish);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
 
-        String cmd = event.getMessage().toLowerCase();
+        if (!event.getMessage().equalsIgnoreCase("/kill")) return;
 
-        // not our command
-        if ((!cmd.startsWith("/kill ")) && (!cmd.equals("/kill"))) {
-            return;
-        }
+        if (event.getPlayer() == null) return;
 
-        Player requester = event.getPlayer();
+        final Player player = event.getPlayer();
 
-        // issuer has to be a player
-        if (requester == null) {
-            return;
-        }
-
-        // if perm is correct, return so /kill $1 command can be used
-        if (requester.hasPermission("minecraft.command.kill")) {
-            return;
-        }
+        // if permission is correct, return, so /kill command can be used normally
+        if (player.hasPermission("minecraft.command.kill")) return;
 
         // cancel original event
         event.setCancelled(true);
 
-        // no kills while cooldown active
-        if (cooldown) {
-            cooldownMessage(requester);
+        // no kill while cooldown active
+        if (cooldowns.contains(player.getUniqueId())) {
+            player.sendMessage(getConfig().getString("cooldown-message"));
             return;
         } else {
-            cooldown = true;
+            cooldowns.add(player.getUniqueId());
         }
 
-        getLogger().info("Killing: " + requester.getName());
+        getLogger().info("Suiciding: " + player.getName());
 
         // dismount
-        Optional<Entity> vehicle = Optional.ofNullable(requester.getVehicle());
-        if (vehicle.isPresent()) {
-            getLogger().info("Dismounting " + requester.getDisplayName() + " from " + vehicle.get().getName() + " before killing");
-            vehicle.get().eject();
-        }
+        Optional
+                .ofNullable(player.getVehicle())
+                .ifPresent(Entity::eject);
 
-
-        // tp exploit protection vanish
-        vanish(requester);
+        // coordinate exploit protection vanish
+        vanish(player);
 
         // kill requester
-        requester.setHealth(0);
+        player.setHealth(0);
         // TODO - EntityDamageEvent.DamageCause.SUICIDE
 
-        // unvanish requester after n ticks
-        new UnvanishRunnable(this, requester).runTaskLater(this, getConfig().getInt("unvanish-ticks"));
-
-    }
-
-    public void disableCooldown() {
-        cooldown = false;
-    }
-
-    public void unvanish(Player player) {
-        for (Player onlinePlayer : getServer().getOnlinePlayers()) {
-            if (!onlinePlayer.equals(player)) {
-                onlinePlayer.showPlayer(this, player);
+        // unvanish
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                unvanish(player);
             }
+        }.runTaskLater(this, getConfig().getInt("unvanish-ticks"));
+
+        // cooldown
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                cooldowns.remove(player.getUniqueId());
+            }
+        }.runTaskLater(this, getConfig().getInt("cooldown-ticks"));
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (vanished.contains(event.getPlayer().getUniqueId())) {
+            unvanish(event.getPlayer());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (vanished.contains(event.getPlayer().getUniqueId())) {
+            unvanish(event.getPlayer());
         }
     }
 
     private void vanish(Player player) {
-        for (Player onlinePlayer : getServer().getOnlinePlayers()) {
-            if (!onlinePlayer.equals(player)) {
-                onlinePlayer.hidePlayer(this, player);
-            }
-        }
+        vanished.add(player.getUniqueId());
+        getServer()
+                .getOnlinePlayers()
+                .stream()
+                .filter(p -> !p.getUniqueId().equals(player.getUniqueId()))
+                .forEach(p -> p.hidePlayer(this, player));
     }
 
-    private void cooldownMessage(Player player) {
-        player.sendMessage(getConfig().getString("cooldown-message"));
+    public void unvanish(Player player) {
+        getServer()
+                .getOnlinePlayers()
+                .stream()
+                .filter(p -> !p.getUniqueId().equals(player.getUniqueId()))
+                .forEach(p -> p.showPlayer(this, player));
+        vanished.remove(player.getUniqueId());
     }
 
     private void config() {
 
-        getConfig().addDefault("cooldown-message", "Sorry, Death is busy at the moment. Please try again later...");
-        getConfig().addDefault("cooldown-ticks", 40);
-        getConfig().addDefault("unvanish-ticks", 20);
+        getConfig().addDefault("cooldown-message", ChatColor.RED + "Sorry, Death is too busy at the moment. Please try again later..." + ChatColor.RESET);
+        getConfig().addDefault("cooldown-ticks", 20);
+        getConfig().addDefault("unvanish-ticks", 10);
         getConfig().options().copyDefaults(true);
 
-        if (getConfig().getInt("unvanish-ticks") <= 20) {
-            getConfig().set("unvanish-ticks", 20);
+        if (getConfig().getInt("unvanish-ticks") <= 10) {
+            getConfig().set("unvanish-ticks", 10);
         }
 
         if (getConfig().getInt("cooldown-ticks") <= getConfig().getInt("unvanish-ticks")) {
